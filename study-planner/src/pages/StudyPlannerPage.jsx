68d618780716e39485c91a6c39664b8214d2aff9
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-06-11 22:05:35 -0300

    implementado funcao de marcar como completada, editar e remover tarefa

--- study-planner/src/pages/StudyPlannerPage.jsx
+++ study-planner/src/pages/StudyPlannerPage.jsx
@@ -1,10 +1,13 @@
-import { useState, useSyncExternalStore } from 'react';
-import { useTasks } from '../contexts/TaskContext';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   selectPendingTasks,
   selectCompletedTasks,
+  toggleTaskComplete,
+  addTask,
   selectTasks,
+  editTask,
+  deleteTask,
 } from '../store/slices/taskSlice';
 import { useTheme } from '../contexts/ThemeContext';
 import { EmptyState, ThemeToggle } from '../components/UI';
@@ -12,22 +15,13 @@ import { AddTaskModal, EditTaskModal } from '../components/Modal';
 import { TaskSection } from '../components/Task';
 
 function StudyPlannerPage() {
+  const dispatch = useDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [taskToEdit, setTaskToEdit] = useState(null);
-
   const tasks = useSelector(selectTasks);
   const pendingTasks = useSelector(selectPendingTasks);
   const completedTasks = useSelector(selectCompletedTasks);
-
-  const {
-    addTask,
-    toggleTaskComplete,
-    editTask,
-    deleteTask,
-    getPendingTasks,
-    getCompletedTasks,
-  } = useTasks();
 
   const handleAddTask = () => {
     setIsModalOpen(true);
@@ -38,11 +32,11 @@ function StudyPlannerPage() {
   };
 
   const handleAddNewTask = (newTask) => {
-    addTask(newTask);
+    dispatch(addTask(newTask));
   };
 
   const handleToggleComplete = (taskId) => {
-    toggleTaskComplete(taskId);
+    dispatch(toggleTaskComplete(taskId));
   };
 
   const handleEditTask = (taskId) => {
@@ -57,11 +51,11 @@ function StudyPlannerPage() {
   };
 
   const handleSaveEditTask = (taskId, updatedTask) => {
-    editTask(taskId, updatedTask);
+    dispatch(editTask({ taskId, updatedTask }));
   };
 
   const handleDeleteTask = (taskId) => {
-    deleteTask(taskId);
+    dispatch(deleteTask(taskId));
   };
 
   const theme = useTheme();
